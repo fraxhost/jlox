7054d9b9446dee378dab02e901d92d080d3673fd
fraxhost <ahmedryanfaiyaz@gmail.com>
2025-10-24 12:42:59 -0500

    feat: 4.6.2 - Number literals; 4.7 Researved Words and Identifiers;

--- Scanner.java
+++ Scanner.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner {
+    private static final Map<String, TokenType> keywords;
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
     private int start = 0;
@@ -10,6 +13,26 @@ public class Scanner {
 
     public Scanner(String source) {
         this.source = source;
+    }
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    TokenType.AND);
+        keywords.put("class",  TokenType.CLASS);
+        keywords.put("else",   TokenType.ELSE);
+        keywords.put("false",  TokenType.FALSE);
+        keywords.put("for",    TokenType.FOR);
+        keywords.put("fun",    TokenType.FUN);
+        keywords.put("if",     TokenType.IF);
+        keywords.put("nil",    TokenType.NIL);
+        keywords.put("or",     TokenType.OR);
+        keywords.put("print",  TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super",  TokenType.SUPER);
+        keywords.put("this",   TokenType.THIS);
+        keywords.put("true",   TokenType.TRUE);
+        keywords.put("var",    TokenType.VAR);
+        keywords.put("while",  TokenType.WHILE);
     }
 
     List<Token> scanTokens() {
@@ -89,8 +112,21 @@ public class Scanner {
             case '"':
                 string();
                 break;
+            case 'o':
+                if (match('r')) {
+                    addToken(TokenType.OR);
+                }
+                break;
             default:
-                Jlox.error(line, "Unexpected character.");
+                if (isDigit(c)) {
+                    number();
+                }
+                else if (isAlpha(c)) {
+                    identifier();
+                }
+                else {
+                    Jlox.error(line, "Unexpected character.");
+                }
                 break;
         }
     }
@@ -102,6 +138,11 @@ public class Scanner {
     private char peek() {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
     }
 
     private char advance() {
@@ -133,6 +174,47 @@ public class Scanner {
         // Trim the surrounding quotes.
         String value = source.substring(start + 1, current - 1);
         addToken(TokenType.STRING, value);
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        String numberString = source.substring(start, current);
+        addToken(TokenType.NUMBER, Double.parseDouble(numberString));
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) {
+            type = TokenType.IDENTIFIER;
+        }
+
+        addToken(TokenType.IDENTIFIER);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 
     private void addToken(TokenType type) {
